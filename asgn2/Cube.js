d7a1e9382a4ff5fbf97b79f6f70c2d640ab5bd61
samhutchins-hue <samhutchins123@gmail.com>
2026-04-26 16:10:18 -0700

    drawWing function and wing rotation sliders

--- asgn2/Cube.js
+++ asgn2/Cube.js
@@ -1,4 +1,3 @@
-// Create a cube buffer object
 let g_cubeBuffer = null;
 
 function initCubeBuffer() {
@@ -7,9 +6,6 @@ function initCubeBuffer() {
     console.log("Failed to create the buffer object");
     return -1;
   }
-  // TODO trying to improve performance while maintaining useability
-  // need to rebind the vertexAttribPointer, and buffer in cube render() function
-  // to allow custom buffers if necessary through drawTriangle3D
   gl.bindBuffer(gl.ARRAY_BUFFER, g_cubeBuffer);
   gl.bufferData(
     gl.ARRAY_BUFFER,
@@ -27,81 +23,7 @@ function initCubeBuffer() {
 }
 
 function drawCube(matrix, color) {
-  let c = new Cube();
-  c.color = color;
-  c.matrix = matrix;
-  c.render();
-}
-
-class Cube {
-  constructor() {
-    this.type = "cube";
-    this.color = [1.0, 1.0, 1.0, 1.0];
-    this.matrix = new Matrix4();
-  }
-
-  render() {
-    let rgba = this.color;
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, g_cubeBuffer);
-    gl.enableVertexAttribArray(a_Position);
-    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
-    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-    gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
-    gl.drawArrays(gl.TRIANGLES, 0, 36);
-
-    // // front of cube
-    // // 000 110 100
-    // // 000 010 110
-    // drawTriangle3D([0, 0, 0, 1, 1, 0, 1, 0, 0])
-    // drawTriangle3D([0, 0, 0, 0, 1, 0, 1, 1, 0])
-    //
-    // // back of cube
-    // // 001 111 101
-    // // 001 011 111
-    // drawTriangle3D([0, 0, 1, 1, 1, 1, 1, 0, 1])
-    // drawTriangle3D([0, 0, 1, 0, 1, 1, 1, 1, 1])
-    //
-    // gl.uniform4f(
-    //   u_FragColor,
-    //   rgba[0] * 0.6,
-    //   rgba[1] * 0.6,
-    //   rgba[2] * 0.6,
-    //   rgba[3],
-    // )
-    // // left side of cube
-    // drawTriangle3D([0, 0, 0, 0, 1, 1, 0, 0, 1])
-    // drawTriangle3D([0, 0, 0, 0, 1, 0, 0, 1, 1])
-    //
-    // // right side of cube
-    // drawTriangle3D([1, 0, 0, 1, 0, 1, 1, 1, 1])
-    // drawTriangle3D([1, 0, 0, 1, 1, 0, 1, 1, 1])
-    //
-    // gl.uniform4f(
-    //   u_FragColor,
-    //   rgba[0] * 0.9,
-    //   rgba[1] * 0.9,
-    //   rgba[2] * 0.9,
-    //   rgba[3],
-    // )
-    //
-    // // top of cube
-    // // 000 110 100
-    // // 000 010 110
-    // drawTriangle3D([0, 1, 0, 0, 1, 1, 1, 1, 1])
-    // drawTriangle3D([0, 1, 0, 1, 1, 1, 1, 1, 0])
-    //
-    // gl.uniform4f(
-    //   u_FragColor,
-    //   rgba[0] * 0.7,
-    //   rgba[1] * 0.7,
-    //   rgba[2] * 0.7,
-    //   rgba[3],
-    // )
-    // // bottom of cube
-    // // 000 100 101
-    // // 000 001 101
-    // drawTriangle3D([0, 0, 0, 1, 0, 0, 1, 0, 1])
-    // drawTriangle3D([0, 0, 0, 0, 0, 1, 1, 0, 1])
-  }
+  gl.uniform4fv(u_FragColor, color);
+  gl.uniformMatrix4fv(u_ModelMatrix, false, matrix.elements);
+  gl.drawArrays(gl.TRIANGLES, 0, 36);
 }
--- asgn2/asgn2.js
+++ asgn2/asgn2.js
@@ -1,4 +1,4 @@
-// TODO [] remove cube class and implement functional way of building and rendering shapes
+// TODO [x] remove cube class and implement functional way of building and rendering shapes
 // TODO [] measure performance by frametime rather than render scene call (bind by refresh rate of monitor)
 //  render calculation seems to be too quick, duration -> 0, so infinite fps
 // TODO [] verify if I really don't need a Cube class for certain animation states
@@ -57,9 +57,15 @@ let g_selectedSize = 5;
 let g_selectedType = POINT;
 let g_globalAngle = 0;
 let g_globalRot = 0;
+let g_globalAnimation = false;
+
+let g_leftShoulderRot = 0;
 let g_leftElbowRot = 0;
 let g_leftWristRot = 0;
-let g_globalAnimation = false;
+
+let g_rightShoulderRot = 0;
+let g_rightElbowRot = 0;
+let g_rightWristRot = 0;
 
 let a_Position;
 let u_FragColor;
@@ -164,6 +170,7 @@ function main() {
 
 let g_startTime = performance.now() / 1000.0;
 let g_seconds = performance.now() / 1000.0 - g_startTime;
+let g_lastFrameTime = performance.now();
 
 // set up actions for the HTML UI elements
 function addActionsForHtmlUI() {
@@ -203,41 +210,61 @@ function addActionsForHtmlUI() {
   };
 
   // slider events
-  document.getElementById("redSlider").oninput = function () {
-    g_selectedColor[0] = parseFloat(this.value) / colorScale;
-    debugLog("r value: ", g_selectedColor[0]);
-  };
-  document.getElementById("greenSlider").oninput = function () {
-    g_selectedColor[1] = parseFloat(this.value) / colorScale;
-    debugLog("g value: ", g_selectedColor[1]);
-  };
-  document.getElementById("blueSlider").oninput = function () {
-    g_selectedColor[2] = parseFloat(this.value) / colorScale;
-    debugLog("b value: ", g_selectedColor[2]);
-  };
-  document.getElementById("sizeSlider").oninput = function () {
-    g_selectedSize = parseFloat(this.value);
-    debugLog("size value: ", g_selectedSize);
-  };
+  // document.getElementById("redSlider").oninput = function () {
+  //   g_selectedColor[0] = parseFloat(this.value) / colorScale
+  //   debugLog("r value: ", g_selectedColor[0])
+  // }
+  // document.getElementById("greenSlider").oninput = function () {
+  //   g_selectedColor[1] = parseFloat(this.value) / colorScale
+  //   debugLog("g value: ", g_selectedColor[1])
+  // }
+  // document.getElementById("blueSlider").oninput = function () {
+  //   g_selectedColor[2] = parseFloat(this.value) / colorScale
+  //   debugLog("b value: ", g_selectedColor[2])
+  // }
+  // document.getElementById("sizeSlider").oninput = function () {
+  //   g_selectedSize = parseFloat(this.value)
+  //   debugLog("size value: ", g_selectedSize)
+  // }
+
   document.getElementById("angleSlider").oninput = function () {
     g_globalAngle = parseFloat(this.value);
     renderScene();
     debugLog("segment step: ", g_globalAngle);
   };
-  document.getElementById("rotSlider").oninput = function () {
-    g_globalRot = parseFloat(this.value);
+
+  // left wing sliders
+  document.getElementById("leftShoulderSlider").oninput = function () {
+    g_leftShoulderRot = parseFloat(this.value);
     renderScene();
-    debugLog("shoulder: ", g_globalRot);
+    debugLog("shoulder: ", g_leftShoulderRot);
   };
-  document.getElementById("elbowSlider").oninput = function () {
+  document.getElementById("leftElbowSlider").oninput = function () {
     g_leftElbowRot = parseFloat(this.value);
     renderScene();
     debugLog("elbow: ", g_leftElbowRot);
   };
-  document.getElementById("wristSlider").oninput = function () {
+  document.getElementById("leftWristSlider").oninput = function () {
     g_leftWristRot = parseFloat(this.value);
     renderScene();
     debugLog("wrist: ", g_leftWristRot);
+  };
+
+  // right wing sliders
+  document.getElementById("rightShoulderSlider").oninput = function () {
+    g_rightShoulderRot = parseFloat(this.value);
+    renderScene();
+    debugLog("shoulder: ", g_rightShoulderRot);
+  };
+  document.getElementById("rightElbowSlider").oninput = function () {
+    g_rightElbowRot = parseFloat(this.value);
+    renderScene();
+    debugLog("elbow: ", g_rightElbowRot);
+  };
+  document.getElementById("rightWristSlider").oninput = function () {
+    g_rightWristRot = parseFloat(this.value);
+    renderScene();
+    debugLog("wrist: ", g_rightWristRot);
   };
 }
 
@@ -294,14 +321,22 @@ function convertCoordinatesToGL(ev) {
 }
 
 function tick() {
+  const now = performance.now();
+  const frameMS = now - g_lastFrameTime;
+  g_lastFrameTime = now;
+
   g_seconds = performance.now() / 1000.0 - g_startTime;
   verboseLog(g_seconds);
 
   // update animation angles
   updateAnimationAngles();
-
   // render everything
   renderScene();
+
+  sendTextToHTML(
+    " ms: " + Math.floor(frameMS) + " fps: " + Math.floor(10000 / frameMS) / 10,
+    "numdot",
+  );
   requestAnimationFrame(tick);
 }
 
@@ -311,14 +346,56 @@ function updateAnimationAngles() {
   }
 }
 
-function renderScene() {
-  var startTime = performance.now();
+const bodyM = new Matrix4();
 
+const purple = [0.4, 0.3, 0.85, 1];
+const white = [0.95, 0.95, 0.95, 1];
+const darkEye = [0.1, 0.1, 0.15, 1];
+const yellow = [1.0, 0.85, 0.25, 1];
+const blue = [0.035, 0.102, 0.184, 1];
+
+function drawWing(rootM, shoulderRot, elbowRot, wristRot) {
+  let ShoulderM = new Matrix4(rootM)
+    .translate(-0.25, 0.3, 0.25)
+    .rotate(shoulderRot, 0, 0, 1);
+
+  // upper wing
+  drawCube(
+    new Matrix4(ShoulderM).scale(0.1, 0.4, 0.4).translate(-0.5, -1, -0.5),
+    blue,
+  );
+
+  let ElbowM = new Matrix4(ShoulderM)
+    .translate(0, -0.4, 0)
+    .rotate(elbowRot, 0, 0, 1);
+
+  // forearm
+  drawCube(
+    new Matrix4(ElbowM).scale(0.1, 0.3, 0.4).translate(-0.5, -1, -0.5),
+    blue,
+  );
+
+  let WristM = new Matrix4(ElbowM)
+    .translate(0, -0.3, 0)
+    .rotate(wristRot, 0, 0, 1);
+
+  // hand/tip
+  drawCube(
+    new Matrix4(WristM).scale(0.12, 0.15, 0.4).translate(-0.5, -1, -0.5),
+    blue,
+  );
+}
+
+function renderScene() {
   // pass the matrix to u_ModelMatrix attribute
   var globalRotMat = new Matrix4().rotate(g_globalAngle, 0, 1, 0);
   gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
 
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  // TODO maybe remove if not rebinding buffer anywhere else
+  gl.bindBuffer(gl.ARRAY_BUFFER, g_cubeBuffer);
+  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
 
   // TODO: add shapes list later
   // let len = g_shapesList.length;
@@ -328,121 +405,92 @@ function renderScene() {
   //TODO REMOVE gl.uniform4f(u_FragColor, 1.0, 1.0, 1.0, 1.0);
   //drawTriangle3D([-1.0, 0.0, 0.0, -0.5, -1.0, 0.0, 0.0, 0.0, 0.0]);
 
-  const purple = [0.4, 0.3, 0.85, 1];
-  const white = [0.95, 0.95, 0.95, 1];
-  const darkEye = [0.1, 0.1, 0.15, 1];
-  const yellow = [1.0, 0.85, 0.25, 1];
+  // root of the penguin's local coordinate system. tilt/scale/translate
+  // the whole creature by changing this one matrix.
 
   // main body
   drawCube(
-    new Matrix4().translate(-0.25, -0.5, 0.0).scale(0.5, 1.0, 0.5),
-    purple,
+    new Matrix4(bodyM).translate(-0.25, -0.5, 0.0).scale(0.5, 1.0, 0.5),
+    blue,
   );
 
   // front detail
   drawCube(
-    new Matrix4().translate(-0.2, -0.55, -0.1).scale(0.4, 0.75, 0.15),
+    new Matrix4(bodyM).translate(-0.2, -0.55, -0.1).scale(0.4, 0.9, 0.15),
     white,
   );
 
   // face
   drawCube(
-    new Matrix4().translate(-0.15, 0.1, -0.08).scale(0.3, 0.28, 0.12),
+    new Matrix4(bodyM).translate(-0.15, 0.1, -0.08).scale(0.3, 0.38, 0.12),
     white,
   );
 
   // left eye
   drawCube(
-    new Matrix4().translate(-0.14, 0.25, -0.12).scale(0.1, 0.1, 0.03),
+    new Matrix4(bodyM).translate(-0.14, 0.3, -0.12).scale(0.1, 0.1, 0.03),
     darkEye,
   );
 
   // left eye pupil
   drawCube(
-    new Matrix4().translate(-0.14, 0.25, -0.12).scale(0.1, 0.1, 0.02),
+    new Matrix4(bodyM).translate(-0.14, 0.35, -0.13).scale(0.04, 0.04, 0.02),
     white,
   );
 
   // right eye
   drawCube(
-    new Matrix4().translate(0.04, 0.25, -0.12).scale(0.1, 0.1, 0.03),
+    new Matrix4(bodyM).translate(0.04, 0.3, -0.12).scale(0.1, 0.1, 0.03),
     darkEye,
+  );
+
+  // right eye pupil
+  drawCube(
+    new Matrix4(bodyM).translate(0.04, 0.35, -0.13).scale(0.04, 0.04, 0.02),
+    white,
   );
 
   // TODO beak
 
-  let leftShoulderFrame = new Matrix4()
-    .translate(-0.25, 0.3, 0.25)
-    .rotate(g_globalRot, 0, 0, 1);
+  const leftWingM = new Matrix4(bodyM);
+  const rightWingM = new Matrix4(bodyM).scale(-1, 1, 1);
 
-  // upper wing
-  drawCube(
-    new Matrix4(leftShoulderFrame)
-      .scale(0.1, 0.4, 0.4)
-      .translate(-0.5, -1, -0.5),
-    purple,
-  );
+  drawWing(leftWingM, g_leftShoulderRot, g_leftElbowRot, g_leftWristRot);
+  drawWing(rightWingM, -g_rightShoulderRot, -g_rightElbowRot, -g_rightWristRot);
 
-  let leftElbowFrame = new Matrix4(leftShoulderFrame)
-    .translate(0, -0.4, 0)
-    .rotate(g_leftElbowRot, 0, 0, 1);
+  // upper right wing
 
-  // forearm
-  drawCube(
-    new Matrix4(leftElbowFrame).scale(0.1, 0.3, 0.4).translate(-0.5, -1, -0.5),
-    purple,
-  );
-
-  let leftWristFrame = new Matrix4(leftElbowFrame)
-    .translate(0, -0.3, 0)
-    .rotate(g_leftWristRot, 0, 0, 1);
-
-  // hand/tip
-  drawCube(
-    new Matrix4(leftWristFrame)
-      .scale(0.12, 0.15, 0.4)
-      .translate(-0.5, -1, -0.5),
-    purple,
-  );
-
-  // right arm
-  drawCube(
-    new Matrix4()
-      .translate(0.25, -0.2, 0.05)
-      .rotate(-g_globalRot, 0, 0, 1)
-      .scale(0.1, 0.55, 0.4),
-    purple,
-  );
+  // // right arm
+  // drawCube(
+  //   new Matrix4()
+  //     .translate(0.25, -0.2, 0.05)
+  //     .rotate(-g_globalRot, 0, 0, 1)
+  //     .scale(0.1, 0.55, 0.4),
+  //   yellow,
+  // );
 
   // left foot
   drawCube(
-    new Matrix4().translate(-0.2, -0.6, -0.2).scale(0.15, 0.08, 0.25),
+    new Matrix4(bodyM).translate(-0.2, -0.6, -0.2).scale(0.15, 0.08, 0.25),
     yellow,
   );
 
   // right foot
   drawCube(
-    new Matrix4().translate(0.05, -0.6, -0.2).scale(0.15, 0.08, 0.25),
+    new Matrix4(bodyM).translate(0.05, -0.6, -0.2).scale(0.15, 0.08, 0.25),
     yellow,
   );
 
   let K = 200.0;
   for (let i = 1; i < K; ++i) {
-    let c = new Cube();
-    c.matrix.translate(-0.8, (1.9 * i) / K - 1.0, 0);
-    c.matrix.rotate(g_seconds * 100, 1, 1, 1);
-    c.matrix.scale(0.1, 0.5 / K, 1.0 / K);
-    c.render();
+    drawCube(
+      new Matrix4()
+        .translate(-0.8, (1.9 * i) / K - 1.0, 0)
+        .rotate(g_seconds * 100, 1, 1, 1)
+        .scale(0.1, 0.5 / K, 1.0 / K),
+      white,
+    );
   }
-
-  var duration = performance.now() - startTime;
-  sendTextToHTML(
-    " ms: " +
-      Math.floor(duration) +
-      " fps: " +
-      Math.floor(10000 / duration) / 10,
-    "numdot",
-  );
 }
 
 function sendTextToHTML(text, htmlID) {
